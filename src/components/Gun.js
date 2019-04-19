5d09f601b6163722e76802355d3a58ea6406020c
David Ross Digital <david.ross@davidrossdigital.com>
2019-04-19 18:37:51 +0100

    updated gun to prioritise targets next to hit

--- src/components/Gun.js
+++ src/components/Gun.js
@@ -5,106 +5,106 @@ export default class Gun extends Component {
 
   state = {
     targets: [
-      { x: 0, y: 0, priority: 1 },
-      { x: 0, y: 1, priority: 1 },
-      { x: 0, y: 2, priority: 1 },
-      { x: 0, y: 3, priority: 1 },
-      { x: 0, y: 4, priority: 1 },
-      { x: 0, y: 5, priority: 1 },
-      { x: 0, y: 6, priority: 1 },
-      { x: 0, y: 7, priority: 1 },
-      { x: 0, y: 8, priority: 1 },
-      { x: 0, y: 9, priority: 1 },
-      { x: 1, y: 0, priority: 1 },
-      { x: 1, y: 1, priority: 1 },
-      { x: 1, y: 2, priority: 1 },
-      { x: 1, y: 3, priority: 1 },
-      { x: 1, y: 4, priority: 1 },
-      { x: 1, y: 5, priority: 1 },
-      { x: 1, y: 6, priority: 1 },
-      { x: 1, y: 7, priority: 1 },
-      { x: 1, y: 8, priority: 1 },
-      { x: 1, y: 9, priority: 1 },
-      { x: 2, y: 0, priority: 1 },
-      { x: 2, y: 1, priority: 1 },
-      { x: 2, y: 2, priority: 1 },
-      { x: 2, y: 3, priority: 1 },
-      { x: 2, y: 4, priority: 1 },
-      { x: 2, y: 5, priority: 1 },
-      { x: 2, y: 6, priority: 1 },
-      { x: 2, y: 7, priority: 1 },
-      { x: 2, y: 8, priority: 1 },
-      { x: 2, y: 9, priority: 1 },
-      { x: 3, y: 0, priority: 1 },
-      { x: 3, y: 1, priority: 1 },
-      { x: 3, y: 2, priority: 1 },
-      { x: 3, y: 3, priority: 1 },
-      { x: 3, y: 4, priority: 1 },
-      { x: 3, y: 5, priority: 1 },
-      { x: 3, y: 6, priority: 1 },
-      { x: 3, y: 7, priority: 1 },
-      { x: 3, y: 8, priority: 1 },
-      { x: 3, y: 9, priority: 1 },
-      { x: 4, y: 0, priority: 1 },
-      { x: 4, y: 1, priority: 1 },
-      { x: 4, y: 2, priority: 1 },
-      { x: 4, y: 3, priority: 1 },
-      { x: 4, y: 4, priority: 1 },
-      { x: 4, y: 5, priority: 1 },
-      { x: 4, y: 6, priority: 1 },
-      { x: 4, y: 7, priority: 1 },
-      { x: 4, y: 8, priority: 1 },
-      { x: 4, y: 9, priority: 1 },
-      { x: 5, y: 0, priority: 1 },
-      { x: 5, y: 1, priority: 1 },
-      { x: 5, y: 2, priority: 1 },
-      { x: 5, y: 3, priority: 1 },
-      { x: 5, y: 4, priority: 1 },
-      { x: 5, y: 5, priority: 1 },
-      { x: 5, y: 6, priority: 1 },
-      { x: 5, y: 7, priority: 1 },
-      { x: 5, y: 8, priority: 1 },
-      { x: 5, y: 9, priority: 1 },
-      { x: 6, y: 0, priority: 1 },
-      { x: 6, y: 1, priority: 1 },
-      { x: 6, y: 2, priority: 1 },
-      { x: 6, y: 3, priority: 1 },
-      { x: 6, y: 4, priority: 1 },
-      { x: 6, y: 5, priority: 1 },
-      { x: 6, y: 6, priority: 1 },
-      { x: 6, y: 7, priority: 1 },
-      { x: 6, y: 8, priority: 1 },
-      { x: 6, y: 9, priority: 1 },
-      { x: 7, y: 0, priority: 1 },
-      { x: 7, y: 1, priority: 1 },
-      { x: 7, y: 2, priority: 1 },
-      { x: 7, y: 3, priority: 1 },
-      { x: 7, y: 4, priority: 1 },
-      { x: 7, y: 5, priority: 1 },
-      { x: 7, y: 6, priority: 1 },
-      { x: 7, y: 7, priority: 1 },
-      { x: 7, y: 8, priority: 1 },
-      { x: 7, y: 9, priority: 1 },
-      { x: 8, y: 0, priority: 1 },
-      { x: 8, y: 1, priority: 1 },
-      { x: 8, y: 2, priority: 1 },
-      { x: 8, y: 3, priority: 1 },
-      { x: 8, y: 4, priority: 1 },
-      { x: 8, y: 5, priority: 1 },
-      { x: 8, y: 6, priority: 1 },
-      { x: 8, y: 7, priority: 1 },
-      { x: 8, y: 8, priority: 1 },
-      { x: 8, y: 9, priority: 1 },
-      { x: 9, y: 0, priority: 1 },
-      { x: 9, y: 1, priority: 1 },
-      { x: 9, y: 2, priority: 1 },
-      { x: 9, y: 3, priority: 1 },
-      { x: 9, y: 4, priority: 1 },
-      { x: 9, y: 5, priority: 1 },
-      { x: 9, y: 6, priority: 1 },
-      { x: 9, y: 7, priority: 1 },
-      { x: 9, y: 8, priority: 1 },
-      { x: 9, y: 9, priority: 1 },
+      { x: 0, y: 0, priority: 1, hit: false, miss: false, },
+      { x: 0, y: 1, priority: 1, hit: false, miss: false, },
+      { x: 0, y: 2, priority: 1, hit: false, miss: false, },
+      { x: 0, y: 3, priority: 1, hit: false, miss: false, },
+      { x: 0, y: 4, priority: 1, hit: false, miss: false, },
+      { x: 0, y: 5, priority: 25, hit: false, miss: false, },
+      { x: 0, y: 6, priority: 25, hit: false, miss: false, },
+      { x: 0, y: 7, priority: 25, hit: false, miss: false, },
+      { x: 0, y: 8, priority: 25, hit: false, miss: false, },
+      { x: 0, y: 9, priority: 1, hit: false, miss: false, },
+      { x: 1, y: 0, priority: 1, hit: false, miss: false, },
+      { x: 1, y: 1, priority: 1, hit: false, miss: false, },
+      { x: 1, y: 2, priority: 1, hit: false, miss: false, },
+      { x: 1, y: 3, priority: 1, hit: false, miss: false, },
+      { x: 1, y: 4, priority: 1, hit: false, miss: false, },
+      { x: 1, y: 5, priority: 1, hit: false, miss: false, },
+      { x: 1, y: 6, priority: 1, hit: false, miss: false, },
+      { x: 1, y: 7, priority: 1, hit: false, miss: false, },
+      { x: 1, y: 8, priority: 1, hit: false, miss: false, },
+      { x: 1, y: 9, priority: 1, hit: false, miss: false, },
+      { x: 2, y: 0, priority: 1, hit: false, miss: false, },
+      { x: 2, y: 1, priority: 1, hit: false, miss: false, },
+      { x: 2, y: 2, priority: 1, hit: false, miss: false, },
+      { x: 2, y: 3, priority: 1, hit: false, miss: false, },
+      { x: 2, y: 4, priority: 1, hit: false, miss: false, },
+      { x: 2, y: 5, priority: 1, hit: false, miss: false, },
+      { x: 2, y: 6, priority: 1, hit: false, miss: false, },
+      { x: 2, y: 7, priority: 1, hit: false, miss: false, },
+      { x: 2, y: 8, priority: 1, hit: false, miss: false, },
+      { x: 2, y: 9, priority: 1, hit: false, miss: false, },
+      { x: 3, y: 0, priority: 1, hit: false, miss: false, },
+      { x: 3, y: 1, priority: 1, hit: false, miss: false, },
+      { x: 3, y: 2, priority: 1, hit: false, miss: false, },
+      { x: 3, y: 3, priority: 1, hit: false, miss: false, },
+      { x: 3, y: 4, priority: 1, hit: false, miss: false, },
+      { x: 3, y: 5, priority: 1, hit: false, miss: false, },
+      { x: 3, y: 6, priority: 1, hit: false, miss: false, },
+      { x: 3, y: 7, priority: 1, hit: false, miss: false, },
+      { x: 3, y: 8, priority: 1, hit: false, miss: false, },
+      { x: 3, y: 9, priority: 1, hit: false, miss: false, },
+      { x: 4, y: 0, priority: 1, hit: false, miss: false, },
+      { x: 4, y: 1, priority: 1, hit: false, miss: false, },
+      { x: 4, y: 2, priority: 1, hit: false, miss: false, },
+      { x: 4, y: 3, priority: 1, hit: false, miss: false, },
+      { x: 4, y: 4, priority: 1, hit: false, miss: false, },
+      { x: 4, y: 5, priority: 1, hit: false, miss: false, },
+      { x: 4, y: 6, priority: 1, hit: false, miss: false, },
+      { x: 4, y: 7, priority: 1, hit: false, miss: false, },
+      { x: 4, y: 8, priority: 1, hit: false, miss: false, },
+      { x: 4, y: 9, priority: 1, hit: false, miss: false, },
+      { x: 5, y: 0, priority: 1, hit: false, miss: false, },
+      { x: 5, y: 1, priority: 1, hit: false, miss: false, },
+      { x: 5, y: 2, priority: 1, hit: false, miss: false, },
+      { x: 5, y: 3, priority: 1, hit: false, miss: false, },
+      { x: 5, y: 4, priority: 1, hit: false, miss: false, },
+      { x: 5, y: 5, priority: 1, hit: false, miss: false, },
+      { x: 5, y: 6, priority: 1, hit: false, miss: false, },
+      { x: 5, y: 7, priority: 1, hit: false, miss: false, },
+      { x: 5, y: 8, priority: 1, hit: false, miss: false, },
+      { x: 5, y: 9, priority: 1, hit: false, miss: false, },
+      { x: 6, y: 0, priority: 1, hit: false, miss: false, },
+      { x: 6, y: 1, priority: 1, hit: false, miss: false, },
+      { x: 6, y: 2, priority: 1, hit: false, miss: false, },
+      { x: 6, y: 3, priority: 1, hit: false, miss: false, },
+      { x: 6, y: 4, priority: 1, hit: false, miss: false, },
+      { x: 6, y: 5, priority: 1, hit: false, miss: false, },
+      { x: 6, y: 6, priority: 1, hit: false, miss: false, },
+      { x: 6, y: 7, priority: 1, hit: false, miss: false, },
+      { x: 6, y: 8, priority: 1, hit: false, miss: false, },
+      { x: 6, y: 9, priority: 1, hit: false, miss: false, },
+      { x: 7, y: 0, priority: 1, hit: false, miss: false, },
+      { x: 7, y: 1, priority: 1, hit: false, miss: false, },
+      { x: 7, y: 2, priority: 1, hit: false, miss: false, },
+      { x: 7, y: 3, priority: 1, hit: false, miss: false, },
+      { x: 7, y: 4, priority: 1, hit: false, miss: false, },
+      { x: 7, y: 5, priority: 1, hit: false, miss: false, },
+      { x: 7, y: 6, priority: 1, hit: false, miss: false, },
+      { x: 7, y: 7, priority: 1, hit: false, miss: false, },
+      { x: 7, y: 8, priority: 1, hit: false, miss: false, },
+      { x: 7, y: 9, priority: 1, hit: false, miss: false, },
+      { x: 8, y: 0, priority: 1, hit: false, miss: false, },
+      { x: 8, y: 1, priority: 1, hit: false, miss: false, },
+      { x: 8, y: 2, priority: 1, hit: false, miss: false, },
+      { x: 8, y: 3, priority: 1, hit: false, miss: false, },
+      { x: 8, y: 4, priority: 1, hit: false, miss: false, },
+      { x: 8, y: 5, priority: 1, hit: false, miss: false, },
+      { x: 8, y: 6, priority: 1, hit: false, miss: false, },
+      { x: 8, y: 7, priority: 1, hit: false, miss: false, },
+      { x: 8, y: 8, priority: 1, hit: false, miss: false, },
+      { x: 8, y: 9, priority: 1, hit: false, miss: false, },
+      { x: 9, y: 0, priority: 1, hit: false, miss: false, },
+      { x: 9, y: 1, priority: 1, hit: false, miss: false, },
+      { x: 9, y: 2, priority: 1, hit: false, miss: false, },
+      { x: 9, y: 3, priority: 1, hit: false, miss: false, },
+      { x: 9, y: 4, priority: 1, hit: false, miss: false, },
+      { x: 9, y: 5, priority: 1, hit: false, miss: false, },
+      { x: 9, y: 6, priority: 1, hit: false, miss: false, },
+      { x: 9, y: 7, priority: 1, hit: false, miss: false, },
+      { x: 9, y: 8, priority: 1, hit: false, miss: false, },
+      { x: 9, y: 9, priority: 1, hit: false, miss: false, },
     ]
   }
 
@@ -124,24 +124,74 @@ export default class Gun extends Component {
 
   }
 
-  handleFireResponse = res => {
-    // TODO: do something with the response
-
-    const shot = res;
-
+  handleMiss = shot => {
     this.setState(state => {
       const targets = state.targets.map(target => {
         if (target.x === shot.x && target.y === shot.y) {
+          target.miss = true;
           target.priority = 0;
           return target;
         } else {
           return target;
         }
       })
-
       return targets;
-
     })
+  }
+
+  handleHit = shot => {
+    this.setState(state => {
+      const targets = state.targets.map(target => {
+
+        // Update this target
+        if (target.x === shot.x && target.y === shot.y) {
+          target.priority = 0;
+          target.hit = true;
+          return target;
+        } else if (
+
+          // Update priority of valid targets next to this one
+          target.priority > 0 &&
+          (
+            (target.x === shot.x + 1 && target.y === shot.y) ||
+            (target.x === shot.x - 1 && target.y === shot.y) ||
+            (target.x === shot.x && target.y === shot.y + 1) ||
+            (target.x === shot.x && target.y === shot.y - 1)
+          )
+        ) {
+          target.priority = 25;
+          return target;
+        } else {
+          return target;
+        }
+      })
+      return targets;
+    })
+  }
+
+  handleSunk = shot => {
+
+  }
+
+
+
+
+
+  handleFireResponse = res => {
+    // TODO: do something with the response
+
+    const shot = res;
+
+    switch (shot.outcome) {
+      case 'hit':
+        this.handleHit(shot);
+        break;
+      case 'miss':
+        this.handleMiss(shot);
+        break;
+      default:
+        console.log('shot disapeared through a worm hole');
+    }
 
   }
 
